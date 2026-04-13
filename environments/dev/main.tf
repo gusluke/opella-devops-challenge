3f86a2bf62c1b1e52a094613d22304c8a447e102
Gustavo <gustavo@estructurabit.com>
2026-04-13 21:26:32 +0200

    feat: add remote backend configuration and terraform lock file

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -7,6 +7,13 @@ terraform {
       version = "~> 3.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "stterraformstateopella"
+    container_name       = "tfstate"
+    key                  = "dev/terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
